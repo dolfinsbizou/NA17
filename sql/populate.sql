4c5c66a56065f5b5e5fbb3691fedc1df5343802b
Charles Herlin <charlerlin@chrlmba.local>
2017-03-30 20:22:16 +0200

    adding sample informations to populate

--- sql/populate.sql
+++ sql/populate.sql
@@ -4,12 +4,26 @@ INSERT INTO Parcelle (sol, superficie) VALUES
 ('argileux', 200),
 ('calcaires', 100),
 ('calcaires', 400),
-('calcaires' 150),
+('calcaires', 150),
 ('argileux', 300),
 ('argileux', 250)
 ;
 
---INSERT INTO Recolte (annee, id_parcelle, nom_cepage, mode_culture, mode_taille, qte_produite) VALUES
+INSERT INTO Cepage (nom, description) VALUES
+('carignan', 'noir'),
+('négrette', 'noir'),
+('merlot', 'noir'),
+('cabernet-sauvignon', 'noir'),
+('chardonnay', 'blanc'),
+('riesling', 'blanc'),
+('muscat', 'blanc')
+;
+
+INSERT INTO Recolte (annee, id_parcelle, nom_cepage, mode_culture, mode_taille, qte_produite) VALUES
+(2010, 1, 'carignan', 'biologique', 'gobelet', 200),
+(2010, 2, 'négrette', 'biodynamique', 'taille chablis', 300),
+(2011, 3, 'merlot', 'conventionnelle', 'guyot', 200)
+;
 
 --INSERT INTO EvenementClimatique (type, date_evenement, intensite) VALUES
 
@@ -25,21 +39,14 @@ INSERT INTO TraitementPhytosanitaire (nom, description) VALUES
 --INSERT INTO AppliqueA (annee_recolte, id_parcelle_recolte, nom_traitement_phytosanitaire, nb_applications) VALUES
 -- annee_recolte shoud be handled at application level
 
-INSERT INTO Cepage (nom, description) VALUES
-('carignan', 'noir'),
-('négrette', 'noir'),
-('trebbiano', 'blanc'),
-('merlot', 'noir'),
-('cabernet-sauvignon', 'noir'),
-('chardonnay', 'blanc'),
-('riesling', 'blanc'),
-('muscat', 'blanc')
-;
 
 INSERT INTO Vin (appellation, annee, acidite, robe, petillant, prix_base, qualite, quantite_dispo) VALUES
 ('Côte du fleuve', 2010, 4, 'rouge brun à brique', no, 10, 16, 200),
 ('Château les Ancêtres', 2011, 7, 'jaune brun', no, 8, 13, 300),
-('Champétillant', 2012, 5, 'rose saumoné', yes, 12, 19, 250)
+('Champétillant', 2012, 5, 'rose saumoné', yes, 12, 19, 250),
+('Pinot rosé', 2010, 3, 'rose orangé', no, 15, 17, 400),
+('Gamay beaujolais', 2015, 4, 'rouge nuances orangées', no, 12, 13),
+('Chardon du Palais', 2013, 3, 'jaune paille', no, 14, 16)
 ;
 
 --INSERT INTO Constitue (annee_recolte, id_parcelle_recolte, appellation_vin, annee_vin, proportion) VALUES
