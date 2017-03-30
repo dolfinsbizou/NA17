fa6a0d0d29208b5d9a3e6449782ab240d65f746c
Dolf RATTMANN <dolfinsbizou@users.noreply.github.com>
2017-03-30 22:06:03 +0200

    Update populate.sql

--- sql/populate.sql
+++ sql/populate.sql
@@ -25,9 +25,16 @@ INSERT INTO Recolte (annee, id_parcelle, nom_cepage, mode_culture, mode_taille, 
 (2011, 3, 'merlot', 'conventionnelle', 'guyot', 200)
 ;
 
---INSERT INTO EvenementClimatique (type, date_evenement, intensite) VALUES
+INSERT INTO EvenementClimatique (type, date_evenement, intensite) VALUES
+('innondation', '2010-12-12', 6),
+('orage', '2011-05-20', 9)
+;
 
---INSERT INTO Touche (annee_recolte, id_parcelle_recolte, type_evenement, date_evenement, degats) VALUES
+INSERT INTO Touche (annee_recolte, id_parcelle_recolte, type_evenement, date_evenement, degats) VALUES
+(2010, 1, 'inondation', '2010-12-12', 3),
+(2010, 2, 'inondation', '2010-12-12', 8),
+(2011, 3, 'orage', '2011-05-20', 1)
+;
 
 INSERT INTO TraitementPhytosanitaire (nom, description) VALUES
 ('insectator', 'Produit insecticide efficace'),
@@ -36,20 +43,24 @@ INSERT INTO TraitementPhytosanitaire (nom, description) VALUES
 ('ferrisator', 'Traitement des carences en fer')
 ;
 
---INSERT INTO AppliqueA (annee_recolte, id_parcelle_recolte, nom_traitement_phytosanitaire, nb_applications) VALUES
--- annee_recolte shoud be handled at application level
-
+INSERT INTO AppliqueA (annee_recolte, id_parcelle_recolte, nom_traitement_phytosanitaire, nb_applications) VALUES
+(2010, 2, 'champignator', 4)
+;
 
 INSERT INTO Vin (appellation, annee, acidite, robe, petillant, prix_base, qualite, quantite_dispo) VALUES
 ('Côte du fleuve', 2010, 4, 'rouge brun à brique', no, 10, 16, 200),
 ('Château les Ancêtres', 2011, 7, 'jaune brun', no, 8, 13, 300),
-('Champétillant', 2012, 5, 'rose saumoné', yes, 12, 19, 250),
+('Champétillant', 2011, 5, 'rose saumoné', yes, 12, 19, 250),
 ('Pinot rosé', 2010, 3, 'rose orangé', no, 15, 17, 400),
-('Gamay beaujolais', 2015, 4, 'rouge nuances orangées', no, 12, 13),
-('Chardon du Palais', 2013, 3, 'jaune paille', no, 14, 16)
 ;
 
---INSERT INTO Constitue (annee_recolte, id_parcelle_recolte, appellation_vin, annee_vin, proportion) VALUES
+INSERT INTO Constitue (annee_recolte, id_parcelle_recolte, appellation_vin, annee_vin, proportion) VALUES
+(2010, 1, 'Côte du fleuve', 2010, 0.2),
+(2010, 2, 'Côte du fleuve', 2010, 0.8),
+(2011, 3, 'Château les Ancêtres', 2011, 1),
+(2011, 3, 'Champétillant', 2011, 1),
+(2010, 1, 'Pinot rosé', 2010, 1)
+;
 
 INSERT INTO Distributeur (nom, marge, type) VALUES
 ('direct', 0, 'direct'),
@@ -61,6 +72,5 @@ INSERT INTO Distributeur (nom, marge, type) VALUES
 ('Coraply', 0.16, 'grande distribution')
 ;
 
---INSERT INTO Vente (quantite, prix_unit, prix_total, appellation_vin, annee_vin, nom_distributeur) VALUES
--- manually enter prix_total ? Or is it computed at insertion ?
+INSERT INTO Vente (quantite, prix_unit, prix_total, appellation_vin, annee_vin, nom_distributeur) VALUES
 
